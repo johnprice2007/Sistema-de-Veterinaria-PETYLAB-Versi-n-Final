5ac5ed541bae208498b968bb8319ddb163173d19
johnprice2007 <leandercustodiocollas2007@gmail.com>
2026-06-30 13:09:45 -0500

    Creacion de interfaz de vacunacion

--- src/main/java/pe/edu/upn/veterinaria/view/VacunacionPanel.java
+++ src/main/java/pe/edu/upn/veterinaria/view/VacunacionPanel.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
- */
+ *///
 package pe.edu.upn.veterinaria.view;
 
 import java.util.List;
